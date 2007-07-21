58e0873b5582664ec299f014d4040db0ffec89e6
Benjamin Kramer <benny.kra@gmail.com>
2007-07-22 00:23:49 +0200

    some eye-candy

--- mirrormeta.py
+++ mirrormeta.py
@@ -14,6 +14,8 @@ elif len(sys.argv) == 4:
 	output = open(sys.argv[3], 'w')
 else:
 	sys.stderr.write('mirrormeta.py distributor file [output]\n')
+	sys.stderr.write('e.g. ./mirrormeta.py sourceforge testproject/test.tar.gz testproject.metalink\n\n')
+	sys.stderr.write('if output is left out, mirrormeta.py will write to stdout.\n')
 	sys.exit(1)
 
 f = open('thirdpartymirrors', 'r')
@@ -29,7 +31,7 @@ del mirrors[0]
 
 doc = xml.dom.minidom.Document()
 metalink = doc.createElementNS('http://www.metalinker.org/', 'metalink')
-metalink.setAttribute('xmlns', 'http://www.metalinker.org/')
+metalink.setAttribute('xmlns', 'http://www.metalinker.org/') # FIXME: force xmlns in a nicer fashion 
 metalink.setAttribute('version', '3.0')
 doc.appendChild(metalink)
 
@@ -37,11 +39,13 @@ files = doc.createElement('files')
 file = doc.createElement('file')
 file.setAttribute('name', path.basename(sys.argv[2]))
 resources = doc.createElement('resources')
+
 for mirror in mirrors:
 	url = doc.createElement('url')
-	url.setAttribute('type', string.split(mirror, '://')[0])
+	url.setAttribute('type', mirror[:string.find(mirror, '://')])
 	url.appendChild(doc.createTextNode(mirror+'/'+sys.argv[2]))
 	resources.appendChild(url)
+
 file.appendChild(resources)
 files.appendChild(file)
 metalink.appendChild(files)
